ad3d31161e334d43ad2bf2fb7ac08622820f7d20
Leandro Bittencourt Candido <leandrobc@MacBook-Pro-de-Leandro.local>
2026-08-13 13:15:21 -0300

    fix: busca de dados

--- src/services/firebase/firebaseConfig.ts
+++ src/services/firebase/firebaseConfig.ts
@@ -10,4 +10,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
+console.log("🔥 CONECTADO AO PROJET ID:", firebaseConfig.projectId);
+
 export const app = initializeApp(firebaseConfig);
